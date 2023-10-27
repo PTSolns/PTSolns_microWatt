a4c5edde4ee73df8dc465fa02e3eb2edb0683c39
PTSolns <115762362+PTSolns@users.noreply.github.com>
2023-10-27 09:25:08 -0600

    Update PTSolns_microWatt.h

--- src/PTSolns_microWatt.h
+++ src/PTSolns_microWatt.h
@@ -1,3 +1,7 @@
+// microWatt Support Library (mSL)
+// Last Update: Oct 27, 2023
+// contact@PTSolns.com
+
 #ifndef PTSolns_microWatt_h
 #define PTSolns_microWatt_h
 #endif
@@ -6,22 +10,24 @@
 
 const int SDA_pin_default = 21; // Default value for microWatt
 const int SCL_pin_default = 22; // Default value for microWatt
-const int LED_buildIn = 13;
+const int LED_buildIn = 13; // Default value for microWatt
 
 class microWatt 
 {
   public:
     
-  uint8_t begin(int LED = LED_buildIn, int number_of_blink = 4, int time_between_blink = 50);
-	void blink(int LED = LED_buildIn, int number_of_blink = 4, int time_between_blink = 50);
+  uint8_t begin(const int LED = LED_buildIn, const int number_of_blink = 4, int time_between_blink = 50);
+  void blink(const int LED = LED_buildIn, const int number_of_blink = 4, int time_between_blink = 50);
   void printPinout();
-  uint8_t setI2Cpins(int SDA_pin = SDA_pin_default, int SCL_pin = SCL_pin_default);
+  uint8_t setI2Cpins(const int SDA_pin = SDA_pin_default, const int SCL_pin = SCL_pin_default);
   void printI2Cpins();
   void printSPIpins();
-  void I2Cscan(int SDA_pin_scan = SDA_pin_default, int SCL_pin_scan = SCL_pin_default);
-
+  void I2Cscan(const int SDA_pin_scan = SDA_pin_default, const int SCL_pin_scan = SCL_pin_default);
+  void fade(const int LED_pin = LED_buildIn, const int PWM_Channel = 0, const int PWM_freq = 500, const int PWM_res = 8, int fade_inc = 5, int time_step = 20);
+  
   private:
 
-  void callWire(int SDA_pin = SDA_pin_default, int SCL_pin = SCL_pin_default);
+  void callWire(const int SDA_pin = SDA_pin_default, const int SCL_pin = SCL_pin_default);
+  void blinkWarning();
 
 };
