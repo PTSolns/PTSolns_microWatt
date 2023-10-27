09c55157ec79af2a97007deae4fb40ccdb1ddf31
PTSolns <115762362+PTSolns@users.noreply.github.com>
2023-10-27 15:58:02 -0600

    Update Fade.ino

--- examples/Fade/Fade.ino
+++ examples/Fade/Fade.ino
@@ -3,11 +3,11 @@
 // contact@PTSolns.com
 //
 // DESCRIPTION
-// This example demonstrates the classic Fade. It is broken into several Parts to show different featrues of the .fade() function.
+// This example demonstrates the classic Fade. It is broken into several parts to show different features of the .fade() function.
 //
 // Part A) The onboard LED is faded in and out using all the default values. Simply compile/upload this sketch as is, and observe the onboard LED.
 // 
-// Part B) The same onboard LED is faded, but the user is encourgaed to change the default settings. Change either fade_inc or time_step below and see the change in onbaord LED.
+// Part B) The same onboard LED is faded, but the user is encouraged to change the default settings. Change either fade_inc or time_step below and see the change in onboard LED.
 //
 // Part C) Let's change the LED_pin to something else. First let's change it to some pin that is *NOT* allowed, e.g. LED_pin = 130. 
 //         Change the LED_pin compile/upload and open Serial Monitor to 115200 baud. Observe the onboard LED pattern also.
@@ -16,7 +16,7 @@
 //         WARNING! Although G1 (TX) and G3 (RX) can be used as PWM pins, but they may interfere with Serial Monitor. Use other pins if possible.
 //         You can also print the pinout diagram and take a look there to see which pins are PWM capable. Uncomment the microWatt.printPinout() in the setup() below and see.
 //        
-// Part D) Finally, change the LED_pin to one of the allowed PWM pins (but not 13, as that's the onboard pin and we want to check one of the exteranl pins).
+// Part D) Finally, change the LED_pin to one of the allowed PWM pins (but not 13, as that's the onboard pin and we want to check one of the external pins).
 //         Connect a LED in series with a 200 ohm to 1k ohm resistor to the external pin you set. Use a breadboard for easy connection.
 // 
 // Tips: To use all the default values, use:
@@ -32,10 +32,10 @@
 
 microWatt microWatt;
 
-const int LED_pin     = 13;   // [Default: 13] Pin 13 is builtin pin.
+const int LED_pin     = 13;   // [Default: 13] Pin 13 is the builtin LED.
 const int PWM_channel = 0;    // [Default: 0, Allowed: 0-15] ESP32 has 16 channels (0 to 15) that can generate 16 independent waveforms
 const int PWM_freq    = 500;  // [Default: 500] PWM frequency, in Hz.
-const int PWM_res     = 8;    // [Default: 8, Allowed: 1-16] Resolution setting in bits. 8 bits = 0 to 2^8-1, 16 bits = 0 to 2^16-1. microWatt has up to 16 bit resolution. 
+const int PWM_res     = 8;    // [Default: 8, Allowed: 1-16] Resolution setting in bits. 8 bits = 0 to 2^8-1, 16 bits = 0 to 2^16-1. microWatt has up to 16-bit resolution. 
 int fade_inc          = 5;    // [Default: 5] The amount the LED changes brightness per iteration. 
 int time_step         = 20;   // [Default: 20] Time between increments, in milliseconds.
 
