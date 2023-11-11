faa42097300abef87496b415e5f8cb8604aa6b0c
PTSolns <115762362+PTSolns@users.noreply.github.com>
2023-11-11 09:25:41 -0700

    Update PTSolns_microWatt.h

--- src/PTSolns_microWatt.h
+++ src/PTSolns_microWatt.h
@@ -8,23 +8,24 @@
 #include <Arduino.h>
 #include <inttypes.h>
 
+#define LED_BUITLIN 13 // Onbaord LED for microWatt
+
 const int SDA_pin_default = 21; // Default value for microWatt
 const int SCL_pin_default = 22; // Default value for microWatt
-const int LED_buildIn = 13; // Default value for microWatt
 
 class microWatt 
 {
   public:
     
-  uint8_t begin(const int LED = LED_buildIn, const int number_of_blink = 4, int time_on_blink = 50, int time_off_blink = 50);
-  void blink(const int LED = LED_buildIn, int number_of_blink = 4, int time_on_blink = 50, int time_off_blink = 50);
-  void blinkDelay(const int LED = LED_buildIn, int number_of_blink = 4, int time_on_blink = 50, int time_off_blink = 50);
+  uint8_t begin(const int LED = LED_BUITLIN, const int number_of_blink = 4, int time_on_blink = 50, int time_off_blink = 50);
+  void blink(const int LED = LED_BUITLIN, int number_of_blink = 4, int time_on_blink = 50, int time_off_blink = 50);
+  void blinkDelay(const int LED = LED_BUITLIN, int number_of_blink = 4, int time_on_blink = 50, int time_off_blink = 50);
   void printPinout();
   uint8_t setI2Cpins(const int SDA_pin = SDA_pin_default, const int SCL_pin = SCL_pin_default);
   void printI2Cpins();
   void printSPIpins();
   void I2Cscan(const int SDA_pin_scan = SDA_pin_default, const int SCL_pin_scan = SCL_pin_default);
-  void fade(const int LED_pin = LED_buildIn, const int PWM_Channel = 0, const int PWM_freq = 500, const int PWM_res = 8, int fade_inc = 5, int time_step = 20);
+  void fade(const int LED_pin = LED_BUITLIN, const int PWM_Channel = 0, const int PWM_freq = 500, const int PWM_res = 8, int fade_inc = 5, int time_step = 20);
   void deepSleep(uint32_t duration);
   void lightSleep(uint32_t duration);
   void setFreq(uint32_t CPUfreq);
